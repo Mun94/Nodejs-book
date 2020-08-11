eb480c1b38f4f7232d8a7b0d60facf8c53c29fdd
Mun94 <seongwoo720@gmail.com>
2020-08-11 16:59:09 +0900

    200811 | DONE | Install sequelize

---
+++ learn-sequelize/models/comment.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "comment",
+    {
+      comment: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
