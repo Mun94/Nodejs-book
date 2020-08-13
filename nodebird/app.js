3c27b592f401370cff640b0a9c9e06332d2c6f00
Mun94 <seongwoo720@gmail.com>
2020-08-13 15:21:54 +0900

    200813 | DONE | 로컬 로그인 구현

--- nodebird/app.js
+++ nodebird/app.js
@@ -4,13 +4,17 @@ const morgan = require("morgan");
 const path = require("path");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+
 require("dotenv").config();
 
 const pageRouter = require("./routes/page");
 const { sequelize } = require("./models");
+const passportConfig = require("./passport/index.js");
 
 const app = express();
 sequelize.sync();
+passportConfig(passport);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -34,7 +38,8 @@ app.use(
 );
 
 app.use(flash());
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use("/", pageRouter);
 
 app.use((req, res, next) => {
