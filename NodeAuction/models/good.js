19a7443d9d1d559cf138507e9517b90b38f469c2
Mun94 <seongwoo720@gmail.com>
2020-08-16 23:26:07 +0900

    200816 | DONE | 프로젝트 구조 갖추기

---
+++ NodeAuction/models/good.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "good",
+    {
+      name: {
+        type: DataTypes.STRING(40),
+        allowNull: false,
+      },
+      img: {
+        type: DataTypes.STRING(200),
+        allowNull: true,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+    },
+    {
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+};
---
+++ NodeAuction/models/user.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "user",
+    {
+      email: {
+        type: DataTypes.STRING(40),
+        allowNull: false,
+        unique: true,
+      },
+      nick: {
+        type: DataTypes.STRING(15),
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+      },
+      money: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+    },
+    {
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+};
